a64c93cde973298e38312b01bf549ccc3f310f2a
abdullahfikri <mfikri.dev@gmail.com>
2022-05-18 01:08:20 +0700

    Retrofit Go App

---
+++ app/src/main/java/com/nsh/goapps/config/ServerConfig.java
@@ -0,0 +1,6 @@
+package com.nsh.goapps.config;
+
+public class ServerConfig {
+    public static final String SERVER_URL = "http://10.0.2.2/goApp/";
+    public static final String API_ENDPOINT = SERVER_URL;
+}
